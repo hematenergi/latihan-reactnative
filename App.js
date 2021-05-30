2ce5f69de1e24c583b6392dbe235e12588219802
hematenergi <danyarkham@gmail.com>
2021-05-30 19:43:30 +0700

    styling component

--- App.js
+++ App.js
@@ -1,10 +1,42 @@
 import React, {Component, Profiler} from 'react';
-import {Image, Text, TextInput, View} from 'react-native';
+import {Image, Text, TextInput, View, StyleSheet} from 'react-native';
 
 // functional component
 const App = () => {
-  return <SampleComponent />;
+  return (
+    <View>
+      {/* <SampleComponent /> */}
+      <StylingReactNativeComponent />
+    </View>
+  );
 };
+
+//styling component
+const StylingReactNativeComponent = () => {
+  return (
+    <View>
+      <Text style={styles.text}>Styling Component</Text>
+      <View
+        style={{
+          width: 100,
+          height: 100,
+          backgroundColor: '#5f27cd',
+          borderWidth: 4,
+          borderColor: '#222f3e',
+          marginTop: 20,
+          marginLeft: 20,
+        }}
+      />
+    </View>
+  );
+};
+
+//styling css component
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 20,
+  },
+});
 
 const SampleComponent = () => {
   return (
